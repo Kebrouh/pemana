9a7b2eda797d7f0df51c812e3f426c6de9e55a9b
Kebrouh <oli1999@hotmail.ca>
2023-07-10 15:01:25 -0400

    fix-categorieData-try2

--- src/components/AccueilSite/AccueilSite.vue
+++ src/components/AccueilSite/AccueilSite.vue
@@ -19,34 +19,28 @@
 </script>
 
 <template>
-    
     <div class="accueil-site">
-
-        <div class="accueil-top">
+      <div class="accueil-top">
+        <div class="wrapImg">
+          <img src="@/assets/img/69-71_00.jpg" alt="lac-sup-principal">
+        </div>
+        <div class="wrapInfo">
+          <div class="info-container">
+            <h1>Pemana Inc.</h1>
+            <h3>Location de quai et de logement</h3>
             <div class="wrapImg">
-                <img src="@/assets/img/69-71_00.jpg" alt="lac-sup-principal">
+              <img src="@/assets/img/logo_pemana_light.svg" alt="logo-light-full">
             </div>
-            <div class="wrapInfo">
-                <div class="info-container">
-                    <h1>Pemana Inc.</h1>
-                    <h3>Location de quai et de logement</h3>
-                    <div class="wrapImg">
-                        <img src="@/assets/img/logo_pemana_light.svg" alt="logo-light-full">
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-
-        <div class="listeVignetteCat" v-if="categorieData">
-            <h2>Explorer nos locations</h2>
-            <VignetteCategorie v-for="categorie in updatedCategorieData" :key="categorie.id"
-                :objCat="categorie" 
-            />
-        </div>
-
+      </div>
+  
+      <div class="listeVignetteCat" v-if="updatedCategorieData">
+        <h2>Explorer nos locations</h2>
+        <VignetteCategorie v-for="categorie in updatedCategorieData" :key="categorie.id" :objCat="categorie" />
+      </div>
     </div>
-
-</template>
+  </template>
 
 <style scoped>
 
