5e64f010522641fc3753d1d45375a8c3b50e2a4b
Kebrouh <oli1999@hotmail.ca>
2023-07-10 11:03:32 -0400

    feature-tryGptImgSolution-goneTerriblyWrong

--- src/components/AccueilSite/VignetteCategorie.vue
+++ src/components/AccueilSite/VignetteCategorie.vue
@@ -1,22 +1,29 @@
 <script setup>
+    import { resolveAsset } from 'vite'
+    import { defineProps } from 'vue'
 
     const props = defineProps({
         objCat : null
     })
 
+    const getImagePath = (img) => {
+    if (img) {
+        return resolveAsset(`./img/${img}`)
+    } else {
+        return resolveAsset('./img/no-img.png')
+    }
+    }
 </script>
 
 <template>
 
     <RouterLink class="vignetteCategorie" :to="props.objCat.link">
         <div class="wrapImg">
-            <img v-if="props.objCat.img" :src="props.objCat.img" :alt="props.objCat.name">
-            <img v-else src="@/assets/img/png/no-img.png" :alt="props.objCat.name">
+        <img :src="getImagePath(props.objCat.img)" :alt="props.objCat.name">
         </div>
-    
         <div class="wrapInfo">
-            <span class="material-symbols-outlined" v-text="props.objCat.icon"></span>
-            <p v-text="props.objCat.name"></p>
+        <span class="material-symbols-outlined" v-text="props.objCat.icon"></span>
+        <p v-text="props.objCat.name"></p>
         </div>
     </RouterLink>
     
