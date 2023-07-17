124269c3fc84f28bda593b0646b1dbf6eec69815
Kebrouh <oli1999@hotmail.ca>
2023-07-17 14:40:34 -0400

    feature-global-pageTransition

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,14 @@
     <HeaderApp />
     <RouterView class="contenu"/>
 
+    <RouterView class="contenu">
+      <Transition 
+        enter-active-class="animate_animated animate__fadeInDown"
+        leave-active-class="animate_animated animate__fadeOutDown"
+        mode="out-in"
+      />
+    </RouterView>
+
   </div>
 </template>
 
