1c18052aac980d2d2cff0ab01ca28783935cb450
Kebrouh <oli1999@hotmail.ca>
2023-07-17 17:33:32 -0400

    feature-aVenir-debut

--- src/components/AffichageData/AffichageAVenir.vue
+++ src/components/AffichageData/AffichageAVenir.vue
@@ -6,12 +6,9 @@
 
     <div class="affichage-a-venir">
 
-        <h1>Page projets à venir</h1>
-        <h2>
-            <span class="material-symbols-outlined">construction</span>
-            En construction
-            <span class="material-symbols-outlined">construction</span>
-        </h2>
+        <div class="grid-a-venir">
+            
+        </div>
 
     </div>
 
---
+++ src/components/AffichageData/AffichageAVenirConstruction.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="affichage-a-venir">
+
+        <h1>Page projets à venir</h1>
+        <h2>
+            <span class="material-symbols-outlined">construction</span>
+            En construction
+            <span class="material-symbols-outlined">construction</span>
+        </h2>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
