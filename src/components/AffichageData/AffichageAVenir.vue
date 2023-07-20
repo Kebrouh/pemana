d7f79441c92a9b4a4a9245cdbab5c4b21234948c
Kebrouh <oli1999@hotmail.ca>
2023-07-20 11:57:36 -0400

    feature-aVenir-miseEnPageBase

--- src/components/AffichageData/AffichageAVenir.vue
+++ src/components/AffichageData/AffichageAVenir.vue
@@ -8,22 +8,25 @@
 
         <div class="grid-a-venir">
             <div class="titre">
-                <h1>Projet à venir</h1>
                 <div class="wrapImg">
-                    <img src="@/assets/img/logo_pemana_light_plein.svg" alt="">
+                    <img src="@/assets/img/a_venir.svg" alt="">
                 </div>
             </div>
             <div class="lac-lilly">
-                <h2>au lac Lilly</h2>
-                <div class="wrapImg">
-                    <img src="@/assets/img/no-img.png" alt="">
+                <div class="wrapTitre">
+                    <h3>Logement au</h3>
+                    <h2>Lac Lilly</h2>
                 </div>
                 <div class="wrapLogo">
-                    <img src="@/assets/img/no-img.png" alt="">
+                    <img src="@/assets/img/logo_chertsey.svg" alt="logo_chertsey">
+                </div>
+                <div class="wrapImg">
+                    <img src="@/assets/img/lac_lilly.jpg" alt="">
                 </div>
             </div>
             <div class="lac-simon">
-                <h2>au lac Simon</h2>
+                <h3>Logement au</h3>
+                <h2>Lac Simon</h2>
                 <div class="wrapImg">
                     <img src="@/assets/img/no-img.png" alt="">
                 </div>
@@ -32,7 +35,8 @@
                 </div>
             </div>
             <div class="et-plus">
-                <h2>D'autres projets à venir prochainement</h2>
+                <h3>Plus de projets</h3>
+                <h2>Prochainement</h2>
                 <div class="wrapImg">
                     <img src="@/assets/img/logo_pemana_light_plein.svg" alt="">
                 </div>
