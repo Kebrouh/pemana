ad482b78f9ec9698d656ab57b6f5650cc94007f5
Kebrouh <oli1999@hotmail.ca>
2023-07-20 09:47:17 -0400

    feature-aVenir-placeholder

--- src/components/AffichageData/AffichageAVenir.vue
+++ src/components/AffichageData/AffichageAVenir.vue
@@ -7,7 +7,36 @@
     <div class="affichage-a-venir">
 
         <div class="grid-a-venir">
-            
+            <div class="titre">
+                <h1>Projet à venir</h1>
+                <div class="wrapImg">
+                    <img src="@/assets/img/logo_pemana_light_plein.svg" alt="">
+                </div>
+            </div>
+            <div class="lac-lilly">
+                <h2>au lac Lilly</h2>
+                <div class="wrapImg">
+                    <img src="@/assets/img/no-img.png" alt="">
+                </div>
+                <div class="wrapLogo">
+                    <img src="@/assets/img/no-img.png" alt="">
+                </div>
+            </div>
+            <div class="lac-simon">
+                <h2>au lac Simon</h2>
+                <div class="wrapImg">
+                    <img src="@/assets/img/no-img.png" alt="">
+                </div>
+                <div class="wrapLogo">
+                    <img src="@/assets/img/no-img.png" alt="">
+                </div>
+            </div>
+            <div class="et-plus">
+                <h2>D'autres projets à venir prochainement</h2>
+                <div class="wrapImg">
+                    <img src="@/assets/img/logo_pemana_light_plein.svg" alt="">
+                </div>
+            </div>
         </div>
 
     </div>
