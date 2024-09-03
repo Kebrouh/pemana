7ce9da4929b9b20d951f906029cc32938ff5f0c1
Kebrouh <oli1999@hotmail.ca>
2024-09-03 14:34:56 -0400

    feature-lacSup-video

--- src/components/AffichageData/AffichageLacSup.vue
+++ src/components/AffichageData/AffichageLacSup.vue
@@ -16,6 +16,14 @@
             <div class="wrapImg">
                 <img src="@/assets/img/logo_pemana_light_plein.svg" alt="">
             </div>
+            <div class="video">
+                <iframe 
+                    class="vid"
+                    src="https://drive.google.com/file/d/160SYng5KIrqu0jjVuswfTNgzy-v_0qob/preview" 
+                    muted="1"
+                    autoplay="1">
+                </iframe>
+            </div>
         </div>
 
         <div class="liste-vignette-logement" v-if="location">
