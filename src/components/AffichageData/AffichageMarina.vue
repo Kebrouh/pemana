0e1256ddd52be3998ef5eac543df2585e251527f
Kebrouh <oli1999@hotmail.ca>
2026-06-05 17:54:50 -0400

    feature-marina-serviceLogement

--- src/components/AffichageData/AffichageMarina.vue
+++ src/components/AffichageData/AffichageMarina.vue
@@ -14,7 +14,7 @@
                     <img src="@/assets/img/logo_pemana_light_plein.svg" alt="">
                 </div>
             </div>
-            <div class="galerie">
+            <div class="galerie">   
                 <div class="wrapImg">
                     <img src="@/assets/img/quai.jpg" alt="">
                 </div>
@@ -24,6 +24,65 @@
                 <div class="wrapLogo">
                     <img src="@/assets/img/logo_marina.svg" alt="">
                 </div>
+                <div class="wrapPrice">
+                    <p>Quai saisonnier à partir de 35$ / pieds</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="marina-service">
+            <div class="liste-service">
+                <h2>Services offerts - <span style="color: #89C5E4;">Marina</span></h2>
+                <ul>
+                    <li>Stationnement réservé à nos clients et leur entourage.</li>
+                    <li>Membre saisonnier de la marina</li>
+                    <li>Électricité 15 ampères</li>
+                    <li>Eau</li>
+                    <li>Surveillance des lieux</li>
+                    <li>Quais de service ou d'accostage temporaire</li>
+                    <li>Activités à proximité</li>
+                </ul>
+            </div>
+            <div class="adresse">
+                <p>692 Chem. des Outaouais, Pointe-Fortune</p>
+                <p>QC J0P 1N0</p>
+                <p>(514) 269-3770</p>
+            </div>
+            <div class="wrapMap">
+                <a class="wheater" href="https://www.meteomedia.com/fr/ville/ca/quebec/carillon/actuelle" target="blank">
+                    <p>Météo</p>
+                    <span class="material-symbols-outlined">cloud</span>
+                </a>
+                <div class="wrapImg">
+                    <img src="@/assets/img/map_zoom_v2.svg" alt="">
+                </div>
+                <a href="https://www.google.com/maps/place/692+Chem.+des+Outaouais,+Pointe-Fortune,+QC+J0P+1N0/@45.5615963,-74.3823692,17z/data=!3m1!4b1!4m6!3m5!1s0x4ccec4fbaea2a129:0xde46e275c0255d68!8m2!3d45.5615926!4d-74.3797889!16s%2Fg%2F11cph362pk?entry=ttu" target="_blank">Directions</a>
+            </div>
+        </div>
+
+        <div class="marina-logement">
+            <div class="wrapImg">
+                <a href="https://creagine.ca/360/pointe/" target="_blank">
+                    <img src="/marina_condo_3d_02.jpg" alt="">
+                </a>
+            </div>
+            <div class="wrapInfo">
+                <h3>Projet de construction près de la marina</h3>
+                <p>À venir : <br> <span>Condos à louer pour long terme</span> <br> 8 x <span>3 1/2</span> &amp; 8 x <span>4 1/2</span> <br><br> &amp; <span>Gîte pour location journalière</span></p>
+            </div>
+            <div class="visite">
+                <a href="https://creagine.ca/360/pointe/" target="_blank">Visite virtuel 3D <span class="material-symbols-outlined">visibility</span> </a>
+            </div>
+        </div>
+
+        <div class="logement-service">
+            <div class="liste-service">
+                <h2>Services offerts - <span style="color: #89C5E4;">Logement</span></h2>
+                <ul>
+                    <li>16 logements (3 ½ &amp; 4 ½)</li>
+                    <li>Stationnement pour chaque unité.</li>
+                    <li>2 bornes de recharge de niveau 2 pour véhicules électriques.</li>
+                </ul>
             </div>
         </div>
 
@@ -48,46 +107,7 @@
             </div>
         </div>
 
-        <div class="marina-logement">
-            <div class="wrapImg">
-                <a href="https://creagine.ca/360/pointe/" target="_blank">
-                    <img src="/marina_condo_3d_02.jpg" alt="">
-                </a>
-            </div>
-            <div class="wrapInfo">
-                <h3>Projet de construction près de la marina</h3>
-                <p>À venir : <br> <span>Condos à louer pour long terme</span> <br> 8 x <span>3 1/2</span> &amp; 8 x <span>4 1/2</span> <br><br> &amp; <span>Gîte pour location journalière</span></p>
-            </div>
-            <div class="visite">
-                <a href="https://creagine.ca/360/pointe/" target="_blank">Visite virtuel 3D <span class="material-symbols-outlined">visibility</span> </a>
-            </div>
-        </div>
 
-        <div class="marina-service">
-            <div class="liste-service">
-                <h2>Services offerts - <span style="color: #89C5E4;">Gîte</span></h2>
-                <ul>
-                    <li>Stationnement réservé à nos clients et leur entourage.</li>
-                    <li>Membre saisonnier de la marina</li>
-                    <li>Électricité 15 ampères</li>
-                    <li>Eau</li>
-                    <li>Surveillance des lieux</li>
-                    <li>Quais de service ou d'accostage temporaire</li>
-                    <li>Activités à proximité</li>
-                </ul>
-            </div>
-            <div class="adresse">
-                <p>692 Chem. des Outaouais, Pointe-Fortune</p>
-                <p>QC J0P 1N0</p>
-                <p>(514) 269-3770</p>
-            </div>
-            <div class="wrapMap">
-                <div class="wrapImg">
-                    <img src="@/assets/img/map_zoom_v2.svg" alt="">
-                </div>
-                <a href="https://www.google.com/maps/place/692+Chem.+des+Outaouais,+Pointe-Fortune,+QC+J0P+1N0/@45.5615963,-74.3823692,17z/data=!3m1!4b1!4m6!3m5!1s0x4ccec4fbaea2a129:0xde46e275c0255d68!8m2!3d45.5615926!4d-74.3797889!16s%2Fg%2F11cph362pk?entry=ttu" target="_blank">Directions</a>
-            </div>
-        </div>
 
     </div>
 
